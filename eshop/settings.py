85aa0ed0c8d7bbc91958ad5bfd16af1346558919
wdcxc <904342189@qq.com>
2017-02-18 00:05:39 +0800

    add .gitignore

--- eshop/settings.py
+++ eshop/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eshop',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
