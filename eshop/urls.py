34338246e7b62f3d9eebf34dc50c2dab407b33b7
wdcxc <904342189@qq.com>
2017-02-18 10:32:37 +0800

    init app：admin & customer & seller

--- eshop/urls.py
+++ eshop/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include('admin.urls'), name='admin'),
+    url(r'^customer/', include('customer.urls'), name='customer'),
+    url(r'^seller/', include('seller.urls'), name='seller'),
+    #    url(r'^admin/', admin.site.urls),
 ]
