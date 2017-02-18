6a18ba714eb211e1920647c5a38f206db057ad58
wdcxc <904342189@qq.com>
2017-02-18 14:49:30 +0800

    add base.html

--- eshop/settings.py
+++ eshop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['static/html',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
