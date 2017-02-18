f2bc0e3c487c04d4602c4985c97294974a0369c3
wdcxc <904342189@qq.com>
2017-02-18 15:59:25 +0800

    更新 customer/login.html 链接，用 url tag 代替

--- customer/urls.py
+++ customer/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login$',views.login),
+    url(r'^login$',views.login,name="login"),
+    url(r'^register',views.register,name="register"),
 ]
