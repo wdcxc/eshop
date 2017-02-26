8763f3d7db2fb7c22a68f76a221ef91a1829cf57
zihang1013 <15603006097@163.com>
2017-02-26 16:57:42 +0800

    增加了首页

--- util/captcha.py
+++ util/captcha.py
@@ -30,7 +30,8 @@ class Captcha(object):
         source = list(string.digits + string.ascii_letters)
         captchaCode = random.sample(source, captchaLen)
         image = Image.new("RGB", (width, height), (255, 255, 255))
-        font = ImageFont.truetype(fontPath, fontSize)
+        fontPath = os.path.dirname(os.path.dirname(__file__))+fontPath
+        font = ImageFont.truetype(os.path.normpath(fontPath), fontSize)
         draw = ImageDraw.Draw(image)
 
         for x in range(0, width):
