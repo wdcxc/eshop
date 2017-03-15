ad0c3baf0c4b5de1f41ad192a6ca8e55497bd913
zihang1013 <15603006097@163.com>
2017-03-15 11:40:14 +0800

    添加付款成功页面

--- app/views/common.py
+++ app/views/common.py
@@ -7,3 +7,18 @@ class CommonView(BaseView):
 
     def index(self,request):
         pass
+
+    def success(self,request):
+        pass
+
+    def introduction(self,request):
+        pass
+
+    def pay(self,request):
+        pass
+
+    def search(self,request):
+        pass
+
+    def shopcart(self,request):
+        pass
