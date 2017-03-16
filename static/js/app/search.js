05ead595f29f4d8dea5c02c678df3e5d2994bd48
zihang1013 <15603006097@163.com>
2017-03-16 17:30:53 +0800

    修改了搜索页面

---
+++ static/js/app/search.js
@@ -0,0 +1,82 @@
+/**
+ * Created by zihang on 2017/3/16.
+ */
+$(document).ready(function() {
+        $("#select1").find("dd").click(function(){
+		$(this).addClass("selected").siblings().removeClass("selected");
+		if ($(this).hasClass("select-all")) {
+			$("#selectA").remove();
+		} else {
+			var copyThisA = $(this).clone();
+			if ($("#selectA").length > 0) {
+				$("#selectA").find("a").html($(this).text());
+			} else {
+				$(".select-result dl").append(copyThisA.attr("id", "selectA"));
+			}
+		}
+	});
+
+	$("#select2").find("dd").click(function() {
+		$(this).addClass("selected").siblings().removeClass("selected");
+		if ($(this).hasClass("select-all")) {
+			$("#selectB").remove();
+		} else {
+			var copyThisB = $(this).clone();
+			if ($("#selectB").length > 0) {
+				$("#selectB").find("a").html($(this).text());
+			} else {
+				$(".select-result dl").append(copyThisB.attr("id", "selectB"));
+			}
+		}
+	});
+
+	$("#select3").find("dd").click(function() {
+		$(this).addClass("selected").siblings().removeClass("selected");
+		if ($(this).hasClass("select-all")) {
+			$("#selectC").remove();
+		} else {
+			var copyThisC = $(this).clone();
+			if ($("#selectC").length > 0) {
+				$("#selectC").find("a").html($(this).text());
+			} else {
+				$(".select-result dl").append(copyThisC.attr("id", "selectC"));
+			}
+		}
+	});
+
+	$("#selectA").on("click", function() {
+		$("#selectA").remove();
+		$("#select1").find(".select-all").addClass("selected").siblings().removeClass("selected");
+	});
+
+	$("#selectB").on("click", function() {
+		$("#selectB").remove();
+		$("#select2").find(".select-all").addClass("selected").siblings().removeClass("selected");
+	});
+
+	$("#selectC").on("click", function() {
+		$("#selectC").remove();
+		$("#select3").find(".select-all").addClass("selected").siblings().removeClass("selected");
+	});
+
+	$(".select dd").on("click", function() {
+		if ($(".select-result dd").length > 1) {
+			$(".select-no").hide();
+			$(".eliminateCriteria").show();
+			$(".select-result").show();
+		} else {
+			$(".select-no").show();
+			$(".select-result").hide();
+		}
+	});
+
+	$(".eliminateCriteria").on("click", function() {
+		$("#selectA").remove();
+		$("#selectB").remove();
+		$("#selectC").remove();
+		$(".select-all").addClass("selected").siblings().removeClass("selected");
+		$(".eliminateCriteria").hide();
+		$(".select-no").show();
+		$(".select-result").hide();
+	});
+});
