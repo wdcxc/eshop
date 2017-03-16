6f7fde224866e8d6fd703d7119d4f61cb3027d03
wdcxc <904342189@qq.com>
2017-03-17 00:36:58 +0800

    update：admin/appadmin（商城首页轮播图管理）

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class CarouselModel(models.Model):
 
     title = models.CharField(max_length=50) # 标题
     show = models.BooleanField(default=False) # 是否显示
-    order = models.IntegerField() # 展示权重（权重越大，展示越前）
+    order = models.IntegerField(default=1) # 展示权重（权重越大，展示越前）
     imgUrl = models.URLField() # 图片链接地址
     linkUrl = models.URLField() # 点击跳转链接
     addTime = models.DateTimeField(auto_now_add=True)
