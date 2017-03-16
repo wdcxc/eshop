91b8c7994c54bd4c372443dbfd135168b679ceb1
wdcxc <904342189@qq.com>
2017-03-16 11:56:33 +0800

    merge

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,10 @@ app_name = "app"
 # common
 urlpatterns = [
     url(r'^common/login$',CommonView.as_view(), name = "login"),
-    url(r'^common/index',CommonView.as_view(), name = "index"),
+    url(r'^common/index$',CommonView.as_view(), name = "index"),
+    url(r'^common/introduction$',CommonView.as_view(), name = "introduction"),
+    url(r'^common/pay$',CommonView.as_view(), name = "pay"),
+    url(r'^common/search$',CommonView.as_view(), name = "search"),
+    url(r'^common/shopcart$',CommonView.as_view(), name = "shopcart"),
+    url(r'^common/success$',CommonView.as_view(), name = "success"),
 ]
