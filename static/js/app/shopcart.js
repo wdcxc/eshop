26b06948061eb299310c4ae2729e8bb1f734088f
zihang1013 <15603006097@163.com>
2017-04-02 14:45:56 +0800

    修改购物车页面

---
+++ static/js/app/shopcart.js
@@ -0,0 +1,33 @@
+/**
+ * Created by zihang on 2017/4/2.
+ */
+$(document).ready(function(){
+    $('.commodity-total-price').text((parseFloat($(this).parents('.table-content').find('.commodity-price').text())*parseInt($(this).parents('.table-content').find('.commodity-price').text())).toFixed(2));
+    /*商品数量加减*/
+    $(".add").click(function () {
+        var number=$(this).siblings(".number");
+        number.val(parseInt(number.val())+1);
+        $(this).parents('.table-content').find('.commodity-total-price').text((parseFloat($(this).parents('.table-content').find('.commodity-price').text())*parseInt(number.val())).toFixed(2));
+        if(parseInt(number.val())!=1){
+            $(this).siblings(".reduce").attr('disabled',false);
+        }
+    });
+
+    $(".reduce").click(function () {
+        var number=$(this).siblings(".number");
+        number.val(parseInt(number.val())-1);
+        $(this).parents('.table-content').find('.commodity-total-price').text((parseFloat($(this).parents('.table-content').find('.commodity-price').text())*parseInt(number.val())).toFixed(2));
+        if(parseInt(number.val())==1){
+           $(this).attr('disabled',true);
+       }
+    });
+    /*全选*/
+    $(".select-all").click(function () {
+        if (this.checked) {
+            $(".checkbox").prop('checked', true);
+        } else {
+            $(".checkbox").prop('checked', false);
+        }
+    });
+
+});
