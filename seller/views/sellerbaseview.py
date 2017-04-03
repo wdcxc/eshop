c736141f911a05d6e05bae2de29a68908de154b0
wdcxc <904342189@qq.com>
2017-04-03 10:30:46 +0800

    fix:app间同时登陆账户导致的错误问题

--- seller/views/sellerbaseview.py
+++ seller/views/sellerbaseview.py
@@ -5,5 +5,5 @@ from util.baseview import BaseView
 class SellerBaseView(BaseView):
     def beforeAction(self, request):
         super(SellerBaseView,self).beforeAction(request)
-        if "user" in request.session:
+        if "user" in request.session and request.session["user"]["app"] == self.request_["appadmin"]:
             self.context["seller"] = SellerModel.objects.get(id=request.session["user"]["id"])
