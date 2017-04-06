6ae452b659d6fb37f1db06768f1bfbdd881d32c6
wdcxc <904342189@qq.com>
2017-04-06 12:50:21 +0800

    fix:订单没有收货地址

--- app/views/order.py
+++ app/views/order.py
@@ -61,7 +61,7 @@ class OrderView(AppBaseView):
         addressId = request.GET.get("addressId")
         order = customer.orders.get(id=id)
 
-        if order.status not in (OrderModel.UNPAY,OrderModel.CANCEL):
+        if order.status not in (OrderModel.UNPAY, OrderModel.CANCEL):
             self.context = {"code": 4, "msg": "订单状态异常", "data": {"id": id}}
         else:
             order.totalPrice = 0
@@ -79,16 +79,19 @@ class OrderView(AppBaseView):
                     order.totalPrice += ordProduct.product.price
             else:
                 # 支付订单
-
-                order.receiveAddress = customer.receiveAddresses.get(id=addressId)
-                order.payTime = datetime.now()
-                order.status = OrderModel.UNSEND
-                order.customerMsg = customerMsg
-                order.save()
-                self.context = {"code": 200, "msg": "订单支付成功", "data": {"id": id}}
+                try:
+                    order.receiveAddress = customer.receiveAddresses.get(id=addressId)
+                except:
+                    self.context = {"code": 4, "msg": "收货地址错误", "data": {"id": id}}
+                else:
+                    order.payTime = datetime.now()
+                    order.status = OrderModel.UNSEND
+                    order.customerMsg = customerMsg
+                    order.save()
+                    self.context = {"code": 200, "msg": "订单支付成功", "data": {"id": id}}
 
     @loginRequire(redirectUrl='/customer/common/login')
-    def cancelOrder(self,request):
+    def cancelOrder(self, request):
         """取消订单"""
         self.response_["type"] = self.RESPONSE_TYPE_JSON
         id = request.GET.get("id")
@@ -98,6 +101,6 @@ class OrderView(AppBaseView):
             order.status = OrderModel.CANCEL
             order.cancelTime = datetime.now()
             order.save()
-            self.context = {"code":200,"msg":"订单取消成功","data":{"id":id}}
+            self.context = {"code": 200, "msg": "订单取消成功", "data": {"id": id}}
         else:
             self.context = {"code": 4, "msg": "订单取消失败,订单已支付", "data": {"id": id}}
