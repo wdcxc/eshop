7a5afd0c613970011d2012ca3e95df7bebb6db58
wdcxc <904342189@qq.com>
2017-04-11 11:32:21 +0800

    update:卖家订单页面显示，收货逻辑处理

--- seller/views/order.py
+++ seller/views/order.py
@@ -1,3 +1,5 @@
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
+
 from models.order import OrderProductModel
 from seller.views.sellerbaseview import SellerBaseView
 from util.baseview import loginRequire
@@ -7,15 +9,45 @@ class OrderView(SellerBaseView):
     @loginRequire()
     def order(self, request):
         """待发货订单"""
-        pass
+        seller = self.context["seller"]
+        ordProducts = OrderProductModel.objects.filter(product__seller=seller,
+                                                       status=OrderProductModel.UNSEND).order_by("-addTime")
+        page = request.GET.get("page")
+        paginator = Paginator(ordProducts, 2)
+        try:
+            self.context["products"] = paginator.page(page)
+        except EmptyPage:
+            self.context["products"] = paginator.page(paginator.num_pages)
+        except PageNotAnInteger:
+            self.context["products"] = paginator.page(1)
 
     def receiveorder(self, request):
         """待收货订单"""
-        pass
+        seller = self.context["seller"]
+        ordProducts = OrderProductModel.objects.filter(product__seller=seller,
+                                                       status=OrderProductModel.UNRECEIVE).order_by("-addTime")
+        page = request.GET.get("page")
+        paginator = Paginator(ordProducts, 2)
+        try:
+            self.context["products"] = paginator.page(page)
+        except EmptyPage:
+            self.context["products"] = paginator.page(paginator.num_pages)
+        except PageNotAnInteger:
+            self.context["products"] = paginator.page(1)
 
     def evaluateorder(self, request):
         """待评价订单"""
-        pass
+        seller = self.context["seller"]
+        ordProducts = OrderProductModel.objects.filter(product__seller=seller,
+                                                       status=OrderProductModel.UNEVALUATE).order_by("-addTime")
+        page = request.GET.get("page")
+        paginator = Paginator(ordProducts, 2)
+        try:
+            self.context["products"] = paginator.page(page)
+        except EmptyPage:
+            self.context["products"] = paginator.page(paginator.num_pages)
+        except PageNotAnInteger:
+            self.context["products"] = paginator.page(1)
 
     @loginRequire()
     def sendProduct(self, request):
@@ -29,4 +61,3 @@ class OrderView(SellerBaseView):
             self.context = {"code": 200, "msg": "商品已发货", "data": {"id": id}}
         else:
             self.context = {"code": 4, "msg": "商品状态出错", "data": {"id": id}}
-
